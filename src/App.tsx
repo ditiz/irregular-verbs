aafad28f8bbed89b861c05af73b8e9b3900c7f3b
Mathieu <mathieu.smarzyk@campus-numerique-in-the-alps.com>
2020-04-06 22:44:40 +0200

    Add best score

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import RandomVerb from "./RandomVerb/RandomVerb";
 
 function App() {
   const [score, setScore] = useState<number>(0);
+  const [maxScore, setMaxScore] = useState<number>(0);
 
   const resetScore = () => setScore(0);
+
+  useEffect(() => {
+    if (maxScore < score) {
+      setMaxScore(max => (max < score ? score : max));
+    }
+  }, [score, setMaxScore])
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Irregular verbs</h1>
-        <small onClick={resetScore}>Score: {score}</small>
+        <small>Score: {score}</small>
+        <small>Meilleur Score: {maxScore} </small>
       </header>
       <main>
         <RandomVerb score={score} setScore={setScore} resetScore={resetScore} />
