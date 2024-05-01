7ab7e08682531a62de5ff1be175041cc83a8266b
Ditiz <mathieu.25565@gmail.com>
2024-05-01 02:02:42 +0200

    fix(quizz-selection): can select quizz

--- src/MenuSelect/MenuSelect.tsx
+++ src/MenuSelect/MenuSelect.tsx
@@ -1,83 +1,102 @@
-import React, { useEffect, useState } from 'react';
-import getQuiz from '../data/getQuiz';
-import getTypesQuiz from '../data/getTypesQuiz';
-import Verbs from '../data/verbs';
-import SelectTypeQuiz from '../MenuSelect/SelectTypeQuiz/SelectTypeQuiz';
-import SelectVerbs from '../MenuSelect/SelectVerbs/SelectVerbs';
-import RandomVerb from '../RandomVerb/RandomVerb';
-import { ITypeQuiz, IVerb } from '../types';
+import React, { useCallback, useEffect, useState } from "react";
+import SelectTypeQuiz from "../MenuSelect/SelectTypeQuiz/SelectTypeQuiz";
+import SelectVerbs from "../MenuSelect/SelectVerbs/SelectVerbs";
+import RandomVerb from "../RandomVerb/RandomVerb";
+import getQuiz from "../data/getQuiz";
+import getTypesQuiz from "../data/getTypesQuiz";
+import Verbs from "../data/verbs";
+import { ITypeQuiz, IVerb } from "../types";
 
 interface IMenuSelectProps {
-    score: number;
-    setScore: React.Dispatch<React.SetStateAction<number>>;
-    resetScore: () => void;
+	score: number;
+	setScore: React.Dispatch<React.SetStateAction<number>>;
+	resetScore: () => void;
 }
 
 interface IQuizElement {
-    id: string;
-    element: JSX.Element;
+	id: string;
+	element: JSX.Element;
 }
 
 const MenuSelect = ({ score, setScore, resetScore }: IMenuSelectProps) => {
-    const [showVerb, setShowVerb] = useState<boolean>(false);
-    const [reload, setReload] = useState(false);
+	const [showVerb, setShowVerb] = useState<boolean>(false);
+	const [reload, setReload] = useState(false);
 
-    const [activeVerbs, setActiveVerbs] = useState<IVerb[]>(Verbs);
+	const [activeVerbs, setActiveVerbs] = useState<IVerb[]>(Verbs);
 
-    const [quiz] = useState<IQuizElement[]>(
-        getQuiz({
-            activeVerbs: Verbs,
-            score,
-            setScore,
-            resetScore,
-            setReload,
-        }),
-    );
+	const [typesQuiz, setTypesQuiz] = useState<ITypeQuiz[]>(getTypesQuiz());
 
-    const [typesQuiz, setTypesQuiz] = useState<ITypeQuiz[]>(getTypesQuiz());
+	const [quiz] = useState<IQuizElement[]>(
+		getQuiz({
+			activeVerbs: Verbs,
+			score,
+			setScore,
+			resetScore,
+			setReload,
+			typesQuiz,
+		}),
+	);
 
-    const [elements, setElements] = useState<JSX.Element[]>(quiz.map((q) => q.element));
+	const [elements, setElements] = useState<JSX.Element[]>(
+		quiz.map((q) => q.element),
+	);
 
-    useEffect(() => {
-        const tq = getQuiz({
-            activeVerbs,
-            score,
-            setScore,
-            resetScore,
-            setReload,
-        });
+	const updateElements = useCallback(() => {
+		const tq = getQuiz({
+			activeVerbs,
+			score,
+			setScore,
+			resetScore,
+			setReload,
+			typesQuiz,
+		});
 
-        setElements(tq.map((el) => el.element));
-    }, [activeVerbs, score, setScore, resetScore, setReload]);
+		setElements(tq.map((el) => el.element));
+	}, [activeVerbs, resetScore, score, setScore, typesQuiz]);
 
-    if (showVerb) {
-        return (
-            <RandomVerb
-                score={score}
-                elements={elements}
-                setShowVerb={setShowVerb}
-                reload={reload}
-                setReload={setReload}
-            />
-        );
-    }
+	useEffect(() => {
+		updateElements();
+	}, [
+		updateElements,
+		typesQuiz,
+		activeVerbs,
+		score,
+		setScore,
+		resetScore,
+		setReload,
+	]);
 
-    const _handleClick = () => {
-        setShowVerb((s) => !s);
-    };
+	if (showVerb) {
+		return (
+			<RandomVerb
+				score={score}
+				elements={elements}
+				setShowVerb={setShowVerb}
+				reload={reload}
+				setReload={setReload}
+			/>
+		);
+	}
 
-    return (
-        <div className="menu">
-            <div className="menu-items">
-                <SelectTypeQuiz typesQuiz={typesQuiz} setTypesQuiz={setTypesQuiz} />
+	const _handleClick = () => {
+		setShowVerb((s) => !s);
+	};
 
-                <SelectVerbs activeVerbs={activeVerbs} setActiveVerbs={setActiveVerbs} />
-            </div>
-            <button onClick={_handleClick} className="button-menu">
-                Passer au test
-            </button>
-        </div>
-    );
+	return (
+		<div className="menu">
+			<div className="menu-items">
+				<SelectTypeQuiz typesQuiz={typesQuiz} setTypesQuiz={setTypesQuiz} />
+
+				<SelectVerbs
+					activeVerbs={activeVerbs}
+					setActiveVerbs={setActiveVerbs}
+				/>
+			</div>
+			<button type="button" onClick={_handleClick} className="button-menu">
+				Passer au test
+			</button>
+		</div>
+	);
 };
 
 export default MenuSelect;
